9a25d01bbeb4bf21ab0024cb0283300c4d1ad222
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-27 18:02:22 -0500

    still missing flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
 
-    if user && user.authentication(params[:password])
-      session[:user.id] = user.id
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       flash[:success] = 'Let your journey begin!'
-      redirect_to '/user/#{current_user.id}'
+      redirect_to "/user/#{current_user.id}"
     else
       flash[:warning] = 'Incorrect username or password'
       redirect_to '/login'
