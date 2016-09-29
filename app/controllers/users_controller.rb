5021603af3be83a1cb64924688f83a4cbb21f42d
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-28 20:52:23 -0500

    user sign up fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:new]
+  before_action :authenticate_user!, except: [:new, :create]
   def new
     @user = User.new
   end
