b0eed93af71b23810eafc6c04163533181d3ae58
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-10-17 19:44:22 -0500

    oh my god drag and drop works! but it still doesnt update the database. babysteps

--- app/assets/javascripts/channels/drag.js
+++ app/assets/javascripts/channels/drag.js
@@ -3,13 +3,14 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("tile", ev.target.id);
+    ev.dataTransfer.setData("tiles", ev.target.id);
+    console.log(ev.dataTransfer.setData);
 }
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("tile");
-    var nodeCopy = document.getElementById(data).cloneNode(true);
-      nodeCopy.id = "newId";
-      ev.target.src = "http://www.clipartkid.com/images/386/old-map-stock-vector-clipart-map-of-world-drawn-onto-old-mottled-47y5gJ-clipart.jpg";
+    var data = ev.dataTransfer.getData("tiles");
+    console.log(data);
+      var newSrc = "/assets/" + data;
+      ev.target.src = newSrc;
 }
