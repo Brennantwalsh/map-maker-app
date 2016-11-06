98fb6dbabfc43232d013c9c85386cde87c895611
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-11-06 11:20:28 -0600

    test to see if it works without sprite animations

--- app/assets/javascripts/maps_controller.js
+++ app/assets/javascripts/maps_controller.js
@@ -1,22 +1,22 @@
-// (function() {
-//   "use strict";
+(function() {
+  "use strict";
 
   
 
-//   angular.module("app").controller("mapsController", function($scope, $http) {
-//     $scope.setup = function() {
-//       $http.get("/tiles.json").then(function(response) {
-//         $scope.tiles = response.data
-//       });
-//     };
+  angular.module("app").controller("mapsController", function($scope, $http) {
+    $scope.setup = function() {
+      $http.get("/tiles.json").then(function(response) {
+        $scope.tiles = response.data
+      });
+    };
 
-//     $scope.showBar = false;
-//     $scope.showTiles = function() {
-//       $scope.showBar = !$scope.showBar;
-//     };
+    $scope.showBar = false;
+    $scope.showTiles = function() {
+      $scope.showBar = !$scope.showBar;
+    };
 
-//   $scope.limitToThis = 10;
-//   });
+  $scope.limitToThis = 10;
+  });
 
 
-// }());
+}());
