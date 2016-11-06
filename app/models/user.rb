32e158ff8f6a5d90086d2002ccf64a7c838e0f0c
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-11-06 11:51:07 -0600

    validations added for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   has_many :journeys, foreign_key: :dm_id
   has_many :participations
   has_many :participated_journeys, through: :participations, source: :journey
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :email, format: { with: /\w+@\w+\.\w{2,3}/i, message: "Not Valid" }
+  validates :username, format: { with: /\w{3, 15}/, message: "must be between 3 and 15 characters in length"}
 end
