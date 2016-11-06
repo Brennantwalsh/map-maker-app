3a2185c27977e3ca829cc1e25fa246bcd11bdc41
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-11-06 13:24:16 -0600

    how about with an images in the asset pipeline

--- app/assets/javascripts/maps_controller.js
+++ app/assets/javascripts/maps_controller.js
@@ -1,24 +1,24 @@
-// (function() {
-//   "use strict";
+(function() {
+  "use strict";
 
   
 
-//   angular.module("app").controller("mapsController", function($scope, $http) {
+  angular.module("app").controller("mapsController", function($scope, $http) {
 
-//     $scope.setup = function() {
-//       $http.get("/tiles.json").then(function(response) {
-//         $scope.tiles = response.data
-//       });
-//     };
+    $scope.setup = function() {
+      $http.get("/tiles.json").then(function(response) {
+        $scope.tiles = response.data
+      });
+    };
 
-//     $scope.showBar = false;
-//     $scope.showTiles = function() {
-//       $scope.showBar = !$scope.showBar;
-//     };
+    $scope.showBar = false;
+    $scope.showTiles = function() {
+      $scope.showBar = !$scope.showBar;
+    };
 
-//   $scope.limitToThis = 10;
+  $scope.limitToThis = 10;
   
-//   });
+  });
 
 
-// }());
+}());
